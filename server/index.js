0d7c51a91741aa06b62b5265ba5f2ab95202a532
unknown <arnavranjan005@gmail.com>
2025-02-21 21:54:25 +0530

    server added

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import router from './routes/user.js'
+import dotenv from 'dotenv'
+import connectToMongo from './Db/connection.js';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// connectToMongo();
+app.use('/',router);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`)
+})
