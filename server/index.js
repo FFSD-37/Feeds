c98071554a0195d960c7f7a63596b12ab3e077a2
FFSD-37 <ffsd.team37@gmail.com>
2025-03-17 13:57:22 +0000

    minor changes

--- server/index.js
+++ server/index.js
@@ -2,8 +2,6 @@ import express from 'express'
 import router from './routes/user.js'
 import dotenv from 'dotenv'
 import path from 'path' 
-import file from 'fs'
-// import connectToMongo from './Db/connection.js';/
 dotenv.config();
 
 const app = express();
@@ -13,7 +11,6 @@ app.use(express.static(path.resolve('../view')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// connectToMongo();
 app.use('/',router);
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`)
