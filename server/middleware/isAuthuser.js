461c154b81006dcf743e02cedc61adb596516178
arnavranjan005 <arnavranjan005@gmail.com>
2025-03-22 21:25:08 +0530

    authentication updated

--- server/middleware/isAuthuser.js
+++ server/middleware/isAuthuser.js
@@ -2,7 +2,9 @@ import { verify_JWTtoken } from "cookie-string-parser";
 
 const isAuthuser = (req, res, next) => {
 
+    try{
     const isAuth=verify_JWTtoken(req.cookies.uuid, process.env.USER_SECRET);
+    
     if(isAuth){
         if(req.url==="/login" || req.url==="/signup" || req.url==="/forgetpass" || req.url==='/'){
             return res.redirect("/home");
@@ -10,6 +12,13 @@ const isAuthuser = (req, res, next) => {
         req.userDetails=isAuth;
         next();
     }else{
+        res.cookie("uuid", "", { maxAge: 1 });
+        if(req.url==="/login" || req.url==="/signup" || req.url==="/forgetpass" || req.url==='/'){
+            return next();
+        }
+        return res.redirect("/login");
+    }
+    }catch(e){
         res.cookie("uuid", "", { maxAge: 1 });
         if(req.url==="/login" || req.url==="/signup" || req.url==="/forgetpass" || req.url==='/'){
             return next();
