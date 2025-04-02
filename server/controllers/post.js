099d22638821c0ea7d96d187e9f60ad26978078f
arnavranjan005 <arnavranjan005@gmail.com>
2025-04-02 21:11:27 +0530

    added post schema and create logic

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -2,8 +2,13 @@ import Post from "../models/postSchema.js";
 
 const handlepostupload=async(req,res)=>{
     try {
+        
+    if(!req.body.title || !req.body.url || !req.body.content || !req.body.author) return res.status(400).json({ err: "All fields are required" });
+
+    const id=`${author}-${Date.now()}`
 
     await Post.insertOne({
+        id,
         title:req.body.title,
         url:req.body.url,
         content:req.body.content,
