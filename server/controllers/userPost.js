0af7d39d6eb29ea28dc0f7e11f8d1e501a027eab
FFSD-37 <ffsd.team37@gmail.com>
2025-05-08 00:27:26 +0530

    video tag added

--- server/controllers/userPost.js
+++ server/controllers/userPost.js
@@ -78,6 +78,7 @@ const handleGetpost=async(req,res)=>{
 const suggestedPost=async(req,res)=>{
     try{
         const createdAt=req.query.createdAt || new Date();
+console.log(req.query);
 
         const userDetails=verify_JWTtoken(req.cookies.uuid, process.env.USER_SECRET);
         if(!userDetails) return res.status(401).json({ err: "Unauthorized" });
@@ -88,6 +89,7 @@ const suggestedPost=async(req,res)=>{
         }).sort({createdAt:-1}).limit(5);
 
         if(!posts) return res.status(404).json({ err: "Post not found" });
+        console.log(posts);
         
         return res.json({posts})
     }
