b484c27b4157cc812c17bc8d268fa8177618fc08
arnavranjan005 <arnavranjan005@gmail.com>
2025-05-08 22:05:46 +0530

    imageKit auth changed

--- server/services/imagKit.js
+++ server/services/imagKit.js
@@ -1,9 +1,9 @@
 import ImageKit from "imagekit";
 
 const imagekit = new ImageKit({
-  urlEndpoint: 'https://ik.imagekit.io/FFSD0037/',
-  publicKey: 'public_wnJ6iUhf4XCA3x6A/XV68fTEU4Y=',
-  privateKey: 'private_2wDdfNX21sEy937VZ2Vqbm1v+PE='
+  urlEndpoint: 'https://ik.imagekit.io/lidyx2zxm/',
+  publicKey: 'public_wbpheuS28ohGGR1W5QtPU+uv/z8=',
+  privateKey: 'private_FkCjTDNipiMr/iAb80jMnflnOPk='
 });
 
 const handleimagKitauth=(req,res)=>{
